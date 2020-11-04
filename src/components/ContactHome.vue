c609c8ec65e4e68d65bcf38a9e78114c74d5e4f0
santiago hazana <santiagohazana@gmail.com>
2020-11-05 00:36:23 +0100

    changes missing image

--- src/components/ContactHome.vue
+++ src/components/ContactHome.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="lg:flex">
       <div class="w-full lg:w-1/2">
-        <img src="/decero.jpg" class="w-full object-cover lg:rounded-l" style="height:450px" />
+        <img src="/img/decero.jpg" class="w-full object-cover lg:rounded-l" style="height:450px" />
       </div>
       <div class="w-full lg:w-1/2 bg-gray-100 px-10 lg:rounded-r">
         <h1 class="text-3xl text-gray-700 font-semibold pt-5">Podemos ayudaros</h1>
