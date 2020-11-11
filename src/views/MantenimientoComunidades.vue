adb9f90ae9a6b9ea0894148894c13f8a84eddf45
santiago hazana <santiagohazana@gmail.com>
2020-11-11 22:42:00 +0100

    New Web with form fixed

---
+++ src/views/MantenimientoComunidades.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="container mx-auto">
+    <div class="lg:flex lg:justify-center lg:pt-24">
+      <div class="w-full lg:w-4/5">
+        <h1
+            class="text-3xl text-red-600 text-center font-medium mt-5 mb-3 lg:text-4xl lg:text-left lg:px-6"
+        >Mantenimiento de Comunidades</h1>
+        <img
+            src="/img/servicios/decoracion-de-interiores.jpg"
+            class="w-full object-cover"
+            style="height: 400px;"
+        />
+        <div class="py-6 px-6 text-gray-900 text-xl font-light leading-relaxed">
+          <p class="mb-5">
+            Gracias a nuestra metodología de trabajo y al equipo
+            altamente cualificado logramos espacios personalizados,
+            que crean sensaciones, emociones, estimulan y armonizan
+            el entorno.
+          </p>
+
+          <!--  <div class="text-center mb-8 lg:hidden">
+             <presupuesto-button></presupuesto-button>
+           </div> -->
+
+          <p class="mb-5 font-medium">
+            Nuestros expertos en interiorismo y estilismo de
+            espacios:
+          </p>
+
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Diseñamos los espacios mediante conceptos contundentes,
+            claros y representativos, sin descuidar su viabilidad a
+            nivel técnico y económico.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Presentamos la propuesta final con los precios
+            detallados para su elección y aprobación.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Dirigimos la obra y coordinamos el trabajo de todos los
+            gremios involucrados.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Cumplimos los plazos entregando el lugar reformado,
+            decorado y amueblado.
+          </p>
+
+          <p
+              class="font-extrabold border-l-4 border-r-4 border-red-500 pl-3 text-xl my-10 p-5 bg-gray-100"
+          >
+            ¡No tienes que ocuparte de nada! Nuestros profesionales
+            se encargarán de todo.
+          </p>
+
+          <!-- <div class="text-center mb-8 lg:hidden">
+            <presupuesto-button></presupuesto-button>
+          </div> -->
+
+          <p class="mb-5 font-medium">
+            Dentro de nuestras opciones podemos proponer 5 estilos
+            bien específicos o adaptarnos a sus necesidades:
+          </p>
+
+          <div class="mb-6">
+            <p class="bg-gray-100 px-6 py-4 rounded-r">
+              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+              <span class="font-medium">Estilo Clásico</span>
+            </p>
+            <p class="px-4 border-l-2 border-gray-100 mt-3">
+              Apuesta por la belleza de la ornamentación y la
+              sofisticación.
+            </p>
+            <img
+                src="/img/decoracion-de-interiores/clasica.jpg"
+                class="w-full object-cover h-full mt-3 rounded"
+            />
+          </div>
+
+          <div class="mb-6">
+            <p class="bg-gray-100 px-6 py-4 rounded-r">
+              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+              <span class="font-medium">Estilo Minimalista</span>
+            </p>
+            <p class="px-4 border-l-2 border-gray-100 mt-3">
+              Permite exprimir los matices de nuestra sensibilidad
+              e inspiración. Ideal para todos los amantes de la
+              simplicidad, serenidad y sobriedad.
+            </p>
+            <img
+                src="/img/decoracion-de-interiores/minimalista.jpg"
+                class="w-full object-cover h-full mt-3 rounded"
+            />
+          </div>
+
+          <div class="mb-6">
+            <p class="bg-gray-100 px-6 py-4 rounded-r">
+              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+              <span class="font-medium">Estilo Rústico</span>
+            </p>
+            <p class="px-4 border-l-2 border-gray-100 mt-3">
+              Permite una gran libertad en la composición
+              decorativa. Se consigue un ambiente relajado alejado
+              de las modas.
+            </p>
+            <img
+                src="/img/decoracion-de-interiores/rustico.jpg"
+                class="w-full object-cover h-full mt-3 rounded"
+            />
+          </div>
+
+          <div class="mb-6">
+            <p class="bg-gray-100 px-6 py-4 rounded-r">
+              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+              <span class="font-medium">Estilo Oriental</span>
+            </p>
+            <p class="px-4 border-l-2 border-gray-100 mt-3">
+              Permite conseguir una zona tranquila y reposada. La
+              pauta más importante para conseguir una decoración
+              al estilo asiático es la serenidad.
+            </p>
+            <img
+                src="/img/decoracion-de-interiores/oriental2.jpg"
+                class="w-full object-cover h-full mt-3 rounded"
+            />
+          </div>
+
+          <div class="mb-6">
+            <p class="bg-gray-100 px-6 py-4 rounded-r">
+              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+              <span class="font-medium">Estilo Inglés</span>
+            </p>
+            <p class="px-4 border-l-2 border-gray-100 mt-3">
+              El elemento esencial es la caoba, madera elegante y
+              fina.
+            </p>
+            <img
+                src="/img/decoracion-de-interiores/ingles.jpg"
+                class="w-full object-cover h-full mt-3 rounded"
+            />
+          </div>
+
+          <p class="my-8 font-medium">
+            Si lo desea puede solicitar presupuesto sin compromiso
+            ni coste alguno.
+          </p>
+
+          <div class="text-center mb-5">
+            <presupuesto-button></presupuesto-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MantenimientoComunidades"
+}
+</script>
+
+<style scoped>
+
+</style>
