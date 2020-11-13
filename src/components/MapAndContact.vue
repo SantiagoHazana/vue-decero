e6699cc589e1b999e3dab171209d0ba50efbd8f2
santiago hazana <santiagohazana@gmail.com>
2020-11-13 11:17:12 +0100

    Changes map in contact

--- src/components/MapAndContact.vue
+++ src/components/MapAndContact.vue
@@ -3,7 +3,7 @@
         <div class="lg:flex">
             <div class="w-full lg:w-1/2">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3203.1520195485496!2d-4.52995878483499!3d36.59864758671898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72fc8f62af8835%3A0x8a7a2991ecee3559!2zQXYuIGRlIEJvbmFuemEsIDIxLCAyOTYzMSBCZW5hbG3DoWRlbmEsIE3DoWxhZ2EsIEVzcGHDsWE!5e0!3m2!1ses!2sar!4v1584571192175!5m2!1ses!2sar"
+                    src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1746.5312162550338!2d-4.529136839384965!3d36.59863905457791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72fd7b6171973b%3A0x8860d1b7f2eacebd!2sDecero%20Proyectos%20Y%20Reformas!5e0!3m2!1ses-419!2ses!4v1605262292627!5m2!1ses-419!2ses"
                     width="100%"
                     height="450"
                     frameborder="0"
