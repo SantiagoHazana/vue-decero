dc25d50934d3fbdcfb705637f3f66f4ce117a913
Santiago Hazana <santiagohazana@gmail.com>
2020-11-21 12:48:52 +0100

    Administracion comunidades started adding info

--- src/views/AdministracionComunidades.vue
+++ src/views/AdministracionComunidades.vue
@@ -4,144 +4,77 @@
       <div class="w-full lg:w-4/5">
         <h1
             class="text-3xl text-red-600 text-center font-medium mt-5 mb-3 lg:text-4xl lg:text-left lg:px-6"
-        >Administracion de Comunidades</h1>
+        >Administración de Comunidades</h1>
         <img
             src="/img/servicios/decoracion-de-interiores.jpg"
             class="w-full object-cover"
             style="height: 400px;"
         />
         <div class="py-6 px-6 text-gray-900 text-xl font-light leading-relaxed">
-          <p class="mb-5">
-            Gracias a nuestra metodología de trabajo y al equipo
-            altamente cualificado logramos espacios personalizados,
-            que crean sensaciones, emociones, estimulan y armonizan
-            el entorno.
-          </p>
-
-          <!--  <div class="text-center mb-8 lg:hidden">
-             <presupuesto-button></presupuesto-button>
-           </div> -->
-
-          <p class="mb-5 font-medium">
-            Nuestros expertos en interiorismo y estilismo de
-            espacios:
+          <p class="mb-5 font-bold">
+            Una empresa dedicada a la gestión y administración de Comunidades de propietarios. Siempre accesible y a
+            disposición para asegurar un buen funcionamiento de la Comunidad.
           </p>
 
           <p class="bg-gray-100 px-6 py-4 rounded mb-2">
             <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-            Diseñamos los espacios mediante conceptos contundentes,
-            claros y representativos, sin descuidar su viabilidad a
-            nivel técnico y económico.
+            Detallados controles contables.
           </p>
           <p class="bg-gray-100 px-6 py-4 rounded mb-2">
             <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-            Presentamos la propuesta final con los precios
-            detallados para su elección y aprobación.
+            Contabilización de los ingresos/gastos & preparación de balances.
           </p>
           <p class="bg-gray-100 px-6 py-4 rounded mb-2">
             <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-            Dirigimos la obra y coordinamos el trabajo de todos los
-            gremios involucrados.
+            Elaboración de presupuestos con sus cuotas de comunidad.
           </p>
           <p class="bg-gray-100 px-6 py-4 rounded mb-2">
             <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-            Cumplimos los plazos entregando el lugar reformado,
-            decorado y amueblado.
+            Realización de los cobros y pagos de la comunidad. incluyendo seguimiento de las devoluciones.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Gestión administrativa: Libro de Actas, custodia, redacción y transcripción de las reuniones de la Comunidad.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Seguimiento permanente de los morosos, reclamando el pago vía correo electrónico y continuas llamadas telefónicas.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Envío de circulares informativas mensuales a todos los propietarios de la Comunidad, detallando el estado de sus cuentas (ingresos y gastos).
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Supervisión del funcionamiento de las instalaciones de la comunidad.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Despacho on-line, servicios de registros y seguimiento de averías.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Servicio de Empresas: fontanería, electricidad, ascensores, cerrajería, otros.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Atención telefónica 24 hs.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Idiomas Español, Italiano e Inglés.
           </p>
 
-          <p
-              class="font-extrabold border-l-4 border-r-4 border-red-500 pl-3 text-xl my-10 p-5 bg-gray-100"
-          >
-            ¡No tienes que ocuparte de nada! Nuestros profesionales
-            se encargarán de todo.
-          </p>
+<!--          <p-->
+<!--              class="font-extrabold border-l-4 border-r-4 border-red-500 pl-3 text-xl my-10 p-5 bg-gray-100"-->
+<!--          >-->
+<!--            ¡No tienes que ocuparte de nada! Nuestros profesionales-->
+<!--            se encargarán de todo.-->
+<!--          </p>-->
 
           <!-- <div class="text-center mb-8 lg:hidden">
             <presupuesto-button></presupuesto-button>
           </div> -->
-
-          <p class="mb-5 font-medium">
-            Dentro de nuestras opciones podemos proponer 5 estilos
-            bien específicos o adaptarnos a sus necesidades:
-          </p>
-
-          <div class="mb-6">
-            <p class="bg-gray-100 px-6 py-4 rounded-r">
-              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-              <span class="font-medium">Estilo Clásico</span>
-            </p>
-            <p class="px-4 border-l-2 border-gray-100 mt-3">
-              Apuesta por la belleza de la ornamentación y la
-              sofisticación.
-            </p>
-            <img
-                src="/img/decoracion-de-interiores/clasica.jpg"
-                class="w-full object-cover h-full mt-3 rounded"
-            />
-          </div>
-
-          <div class="mb-6">
-            <p class="bg-gray-100 px-6 py-4 rounded-r">
-              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-              <span class="font-medium">Estilo Minimalista</span>
-            </p>
-            <p class="px-4 border-l-2 border-gray-100 mt-3">
-              Permite exprimir los matices de nuestra sensibilidad
-              e inspiración. Ideal para todos los amantes de la
-              simplicidad, serenidad y sobriedad.
-            </p>
-            <img
-                src="/img/decoracion-de-interiores/minimalista.jpg"
-                class="w-full object-cover h-full mt-3 rounded"
-            />
-          </div>
-
-          <div class="mb-6">
-            <p class="bg-gray-100 px-6 py-4 rounded-r">
-              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-              <span class="font-medium">Estilo Rústico</span>
-            </p>
-            <p class="px-4 border-l-2 border-gray-100 mt-3">
-              Permite una gran libertad en la composición
-              decorativa. Se consigue un ambiente relajado alejado
-              de las modas.
-            </p>
-            <img
-                src="/img/decoracion-de-interiores/rustico.jpg"
-                class="w-full object-cover h-full mt-3 rounded"
-            />
-          </div>
-
-          <div class="mb-6">
-            <p class="bg-gray-100 px-6 py-4 rounded-r">
-              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-              <span class="font-medium">Estilo Oriental</span>
-            </p>
-            <p class="px-4 border-l-2 border-gray-100 mt-3">
-              Permite conseguir una zona tranquila y reposada. La
-              pauta más importante para conseguir una decoración
-              al estilo asiático es la serenidad.
-            </p>
-            <img
-                src="/img/decoracion-de-interiores/oriental2.jpg"
-                class="w-full object-cover h-full mt-3 rounded"
-            />
-          </div>
-
-          <div class="mb-6">
-            <p class="bg-gray-100 px-6 py-4 rounded-r">
-              <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
-              <span class="font-medium">Estilo Inglés</span>
-            </p>
-            <p class="px-4 border-l-2 border-gray-100 mt-3">
-              El elemento esencial es la caoba, madera elegante y
-              fina.
-            </p>
-            <img
-                src="/img/decoracion-de-interiores/ingles.jpg"
-                class="w-full object-cover h-full mt-3 rounded"
-            />
-          </div>
 
           <p class="my-8 font-medium">
             Si lo desea puede solicitar presupuesto sin compromiso
