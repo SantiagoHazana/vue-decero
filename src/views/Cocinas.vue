853f5304a3e54fb76365a0868d7c9c7f238594af
santiago hazana <santiagohazana@gmail.com>
2020-12-01 15:52:01 +0100

    Added video and new logo

--- src/views/Cocinas.vue
+++ src/views/Cocinas.vue
@@ -5,6 +5,7 @@
         <h1 class="text-3xl text-red-600 text-center font-medium mt-5 mb-3 lg:text-4xl lg:text-left lg:px-6">
           Cocinas
         </h1>
+          <iframe class="m-auto p-5 border-2 " width="800" height="488" src="https://www.youtube.com/embed/RsvIZ4NJsvY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   </div>
