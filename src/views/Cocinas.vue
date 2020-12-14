b0e15f58c93cfdba88a1ed680bb574383512778c
santiago hazana <santiagohazana@gmail.com>
2020-12-14 20:42:30 +0100

    Changes in cocinas

--- src/views/Cocinas.vue
+++ src/views/Cocinas.vue
@@ -5,8 +5,31 @@
         <h1 class="text-3xl text-red-600 text-center font-medium mt-5 mb-3 lg:text-4xl lg:text-left lg:px-6">
           Cocinas
         </h1>
-          <iframe class="m-auto p-5 border-2 " width="800" height="488" src="https://www.youtube.com/embed/RsvIZ4NJsvY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="m-auto p-5 border-2 mb-5" width="800" height="488"
+                src="https://www.youtube.com/embed/RsvIZ4NJsvY" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        <div class="py-6 px-6 text-gray-900 text-xl font-light leading-relaxed">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet commodo ex, vitae maximus lectus.
+            Pellentesque eu ex elit.
+            Fusce non elit ut libero varius pellentesque. Nunc vestibulum consectetur ultrices. Aenean eleifend sit amet
+            sapien ac vehicula.
+            Fusce scelerisque, nulla quis maximus elementum, magna nunc tempor ipsum, elementum facilisis augue nisi id
+            metus. Suspendisse quis
+            ipsum eget sapien pellentesque congue. Sed quis bibendum tortor.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p class="bg-gray-100 px-6 py-4 rounded mb-2">
+            <i class="fas fa-check-square text-green-600 fa-lg pr-1"></i>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
       </div>
+
     </div>
   </div>
 
@@ -18,13 +41,13 @@ import ContactForm from "@/components/ContactForm";
 import ChatButton from "@/components/ChatButton";
 
 export default {
-    components: { PresupuestoButton, ContactForm, ChatButton },
-    metaInfo: {
-        title: 'Cocinas',
-        meta: [
-            { name: 'description', content: 'Decero' }
-        ]
-    }
+  components: {PresupuestoButton, ContactForm, ChatButton},
+  metaInfo: {
+    title: 'Cocinas',
+    meta: [
+      {name: 'description', content: 'Decero'}
+    ]
+  }
 };
 </script>
 
